5c62104e280ad612827bcbc9f5cc542927b2db6e
Saliou <gayesaliou29@gmail.com>
2022-05-21 15:10:06 +0200

    fix(app): fix issue #21

--- app/lib/ui/home/home.dart
+++ app/lib/ui/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:letsdrop/blocs/drops/drops_bloc.dart';
 import 'package:letsdrop/constants/routes.dart';
 import 'package:letsdrop/models/user.dart';
 import 'package:letsdrop/utils/addVerticalSpace.dart';
@@ -12,23 +14,34 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-          child: Scaffold(
+      child: BlocBuilder<DropsBloc, DropsState>(
+        builder: (context, state) {
+          return Scaffold(
             backgroundColor: Theme.of(context).backgroundColor,
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    addVerticalSpace(20),
-                    Appbar(avatar: user.profileImage,),
-                    addVerticalSpace(10),
-                    Text(
-                       "${_getAppBarGreeting()} ${user.name}",
-                      style: Theme.of(context).textTheme.headline1,
-                    ),
-                    addVerticalSpace(20),
-                    const DropList(),
-                  ],
+            body: RefreshIndicator(
+              onRefresh: () {
+                context.read<DropsBloc>().add(LoadDrops());
+
+                return Future.delayed(const Duration(seconds: 1));
+              },
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      addVerticalSpace(20),
+                      Appbar(
+                        avatar: user.profileImage,
+                      ),
+                      addVerticalSpace(10),
+                      Text(
+                        "${_getAppBarGreeting()} ${user.name}",
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      addVerticalSpace(20),
+                      const DropList(),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -38,23 +51,23 @@ class Home extends StatelessWidget {
               },
               child: const Icon(Icons.add),
             ),
-          ),
-        );
+          );
+        },
+      ),
+    );
   }
-
 
   String _getAppBarGreeting() {
     final now = DateTime.now();
 
-    if(now.hour >= 6 && now.hour <= 12) {
+    if (now.hour >= 6 && now.hour <= 12) {
       return "Good morning";
     }
 
-    if(now.hour > 12 && now.hour <= 19) {
+    if (now.hour > 12 && now.hour <= 19) {
       return "Good afternoon";
     }
 
     return "Good evening";
-
   }
 }
