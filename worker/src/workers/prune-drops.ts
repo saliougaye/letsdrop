c51e0bf78d516297d2b02ddf7077225e5a07bf3b
Saliou <gayesaliou29@gmail.com>
2022-05-23 14:09:50 +0200

    fix(worker): fix worker connection

--- worker/src/workers/prune-drops.ts
+++ worker/src/workers/prune-drops.ts
@@ -2,6 +2,7 @@ import { Job, Queue, Worker, QueueScheduler } from 'bullmq';
 import config from '../config';
 import DropService from '../services/dropService';
 import Logger from '../services/logger';
+import IORedis from 'ioredis';
 
 const PRUNE_QUEUE_NAME = 'prune-drops-queue';
 const PRUNE_WORKER_NAME = 'prune-drops-worker';
@@ -33,16 +34,18 @@ const instantiatePruneDropsWorker = async () => {
         
     }
 
+    const connection = new IORedis(config.redisUrl);
+
     const pruneDropsQueueScheduler = new QueueScheduler(PRUNE_QUEUE_NAME, {
-        connection: config.redisUrl
+        connection: connection
     });
 
     const pruneDropsQueue = new Queue(PRUNE_QUEUE_NAME, {
-        connection: config.redisUrl
+        connection: connection
     });
 
     const pruneDropsWorker = new Worker(PRUNE_QUEUE_NAME, workerFunction, {
-        connection: config.redisUrl
+        connection: connection
     });
 
     pruneDropsWorker.on('completed', workerOnCompleted);
