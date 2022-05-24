35155b2ab5bb25cc07898bbe539995e635b919cd
Saliou <gayesaliou29@gmail.com>
2022-05-24 09:11:47 +0200

    style(app): change dark color scheme (close #13)

--- app/lib/constants/colors.dart
+++ app/lib/constants/colors.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:letsdrop/utils/hex_to_color.dart';
 
 class AppColors {
   AppColors._();
 
-  static const Color darkShadeBlueMagenta = Color(0xFF393341);  // #393341
-  static const Color darkBlue = Color(0xFF364073); // #364073
-  static const Color skyBlue = Color(0xFF7a7ff4); // #7a7ff4
-  static const Color skyBlueViolet = Color(0xFF9591f2); // #9591f2
-  static const Color skyViolet = Color(0xFFcaacf2); // #caacf2
+  static Color darkPrimaryColor = hexToColor("#1C0C5B");  // #393341
+  static Color darkSecondaryColor = hexToColor("#3D2C8D"); // #364073
+  static Color darkThirdiaryColor = hexToColor("#916BBF"); // #7a7ff4
+  static Color darkFourthColor = hexToColor("#C996CC"); // #9591f2
+  static Color darkFifthColor = hexToColor("#142e1f"); // #caacf2
 
-  static const Color skyGrey = Color(0xFFdde2e8); // #dde2e8
-  static const Color lightSeaGreen = Color(0xFFa9c0c6); //#a9c0c6
-  static const Color seaGreen = Color(0xFF8bb7b6); //#8bb7b6
-  static const Color greenBlue = Color(0xFF005f75); //#005f75
-  static const Color darkGreenBlue = Color(0xFF184c57); //#184c57
+  static Color lightFifthColor = hexToColor("#dde2e8"); // #dde2e8
+  static Color lightFourthColor = hexToColor("#a9c0c6"); //#a9c0c6
+  static Color lightThirdiaryColor = hexToColor("#8bb7b6"); //#8bb7b6
+  static Color lightSecondaryColor = hexToColor("#005f75"); //#005f75
+  static Color lightPrimaryColor = hexToColor("#184c57"); //#184c57
   
 }
